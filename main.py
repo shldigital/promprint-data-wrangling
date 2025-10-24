13bc3a56951c16d2721d3c7255bf837f4f6a13ea
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-10-24 15:43:14 +0100

    [wip] extract circa dates to separate series/file

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ def main():
     ]
     file_path = Path(args.filename)
     df = pd.read_csv(file_path, sep='\t', names=labels)
+
     df = df.map(lambda x: x.split(':')[1].rstrip('/').strip())
+
+    circa_re = r'c(?:a\.?|irca|) ?(?P<circa_date>\d{2,4})'
+    circa_dates = df['Date'].str.extract(circa_re)
+    print(circa_dates)
+    circa_dates.to_csv(file_path.stem + '_dates' + file_path.suffix,
+                       sep='\t',
+                       index=False)
+
     df.to_csv(file_path.stem + '_clean' + file_path.suffix,
               sep='\t',
               index=False)
