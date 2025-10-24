e369ba5298676cdd6f8e1300fd30fd84364e69bf
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-10-24 10:21:42 +0100

    [feat] Transform from rows of dicts to columnar format

---
+++ README.md
@@ -0,0 +1,8 @@
+# Data cleaning for promprint project
+First run is for sample data found at the national library of Scotland:
+
+https://data.nls.uk/data/metadata-collections/catalogue-published-material/
+
+Priority is to clean the dates so that we can filter down to ones that are relevant to specific copyright registers.
+
+We can import into the database directly from there, but at some point we should also clean the author column for more robust checking of a match.
---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+import pandas as pd
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='Input file in tsv format')
+    args = parser.parse_args()
+
+    labels = [
+        'Title', 'Creator', 'Type', 'Publisher', 'Date', 'Language', 'Format',
+        'Relation', 'Rights', 'Identifier', 'Description', 'Subject',
+        'Coverage', 'Contributor', 'Source'
+    ]
+    file_path = Path(args.filename)
+    df = pd.read_csv(file_path, sep='\t', names=labels)
+    df = df.map(lambda x: x.split(':')[1].rstrip('/').strip())
+    df.to_csv(file_path.stem + '_clean' + file_path.suffix,
+              sep='\t',
+              index=False)
+
+
+if __name__ == "__main__":
+    main()
