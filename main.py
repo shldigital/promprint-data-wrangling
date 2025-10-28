ee138957cf74792ba955ba03d98a4c270868b2e9
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-10-28 15:53:10 +0000

    [feature] Add min/max date and types to raw tsv

--- main.py
+++ main.py
@@ -18,13 +18,17 @@ def main():
     df = pd.read_csv(file_path, sep='\t', names=labels)
 
     df = df.map(lambda x: x.split(':')[1].rstrip('/').strip())
+    df.to_csv(file_path.stem + '_columnar' + file_path.suffix,
+              sep='\t',
+              index=False)
+
     df_len = len(df)
     print(f'No of entries: {df_len}')
 
+    # Separate out different types of date in case they're relevant
     dates_re = (r'(?:c(?:a\.?|irca|) ?(?P<circa_date>\d{2,4})|'
                 r'(?P<question_date>\d{2,4})\?|'
                 r'(?P<unqualified_date>\d{2,4}))')
-    # TODO: Check consistency of table - are there NAs? Why?
     dates_df = df['Date'].str.extractall(dates_re).astype('float64')
     dates_df.to_csv(
         file_path.stem + '_dates' + file_path.suffix,
@@ -41,9 +45,13 @@ def main():
     print(f'No. of circa marked dates: {n_cd}')
 
     min_uq_dates = dates_df.groupby(level=0).min().rename(
-        columns={'unqualified_date': 'min_uq_date'}).dropna()
+        columns={
+            'unqualified_date': 'min_uq_date'
+        }).dropna()
     max_uq_dates = dates_df.groupby(level=0).max().rename(
-        columns={'unqualified_date': 'max_uq_date'}).dropna()
+        columns={
+            'unqualified_date': 'max_uq_date'
+        }).dropna()
 
     processed_dates = pd.DataFrame(
         np.nan,
@@ -54,10 +62,22 @@ def main():
     processed_dates.update(min_uq_dates['min_uq_date'])
     processed_dates.update(max_uq_dates['max_uq_date'])
 
+    date_range = pd.DataFrame(np.nan,
+                              index=range(df_len),
+                              columns=['min_date', 'max_date'])
+
+    # NB: Effectively ignoring different date types for now
+    date_range['min_date'] = processed_dates.min(axis=1)
+    date_range['max_date'] = processed_dates.max(axis=1)
+
+    processed_dates = processed_dates.join(date_range)
     processed_dates.to_csv(
         file_path.stem + '_processed_dates' + file_path.suffix,
         sep='\t',
     )
+
+    df = pd.concat(
+        [df.loc[:, :'Date'], processed_dates, df.loc[:, 'Language':]], axis=1)
 
     df.to_csv(file_path.stem + '_clean' + file_path.suffix,
               sep='\t',
