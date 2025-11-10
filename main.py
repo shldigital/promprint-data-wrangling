b607a8a3e7317676cd6ca1ac0c57841db36c50ae
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-11-10 15:32:01 +0000

    [refactor] Split cleaning into sub-functions

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import argparse
-from functools import partial
 import glob
 import logging
 import numpy as np
 import os
 import pandas as pd
+import re
+
+from functools import partial
 from pathlib import Path
+from typing import Tuple
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
@@ -27,39 +30,52 @@ def _labelled_file(out_dir: os.PathLike, file_path: os.PathLike,
     return out_dir / new_name
 
 
-def clean_nls_dates(df: pd.DataFrame, file_path: os.PathLike,
-                    filter_date: float,
-                    debug: bool) -> tuple[pd.DataFrame, pd.DataFrame]:
+def _remove_metadata(title_string: str) -> str:
     """
-    Clean and process the dates of the dataset format provided by
-    National Library of Scotland. This is 'dictionary' style format, with tab
-    separated sets of key-value pairs for each entry. Each entry is then
-    separated by new lines, e.g.
+    Remove strings and numbers not directly related to the title of the entry
+    """
+    square_brackets_clean = re.sub(
+        r'\[(?:microform|illustrated|a novel|plates)\]', '',
+        title_string.lower())
+    editions_clean = re.sub(r'\b(?:n|ed|vol(?:s|ume|umes|))\b', '',
+                            square_brackets_clean)
+    return re.sub(r'\d{1,4}', '', editions_clean)
+
+
+def _clean_title_string(title_string: str) -> str:
+    """
+    Remove/replace ampersands, apostrophes and multi-spaces
+    """
+    no_ampersand = re.sub(r'(&amp;|&)', 'and', title_string)
+    no_apostrophe = re.sub(r"['`]", '', no_ampersand)
+    alphanum = re.sub(r'[^a-zA-Z0-9]', ' ', no_apostrophe)
+    single_spaced = re.sub(r'\s{2,}', ' ', alphanum)
+    return single_spaced.strip().lower()
+
+
+def columnise_nls_data(df: pd.DataFrame, file_path: os.PathLike,
+                       debug: bool) -> pd.DataFrame:
+    """
+    Reformat data provided by National Library of Scotland.
+    This is 'dictionary' style format, with tab separated sets of key-value
+    pairs for each entry. Each entry is then separated by new lines, e.g.
 
     Title: {first entry title}<tab>Creator: {entry creator}<tab>...<newline>
     Title: {second entry title}<tab>Creator: {second entry creator}<tab>...<newline>
 
-    Saves data to tsv file in columnar format (one column per key) e.g.
+    Returns a dataframe in columnar format (one column per key) e.g.
 
     Title<tab>Creator<tab>...
     {first entry title}<tab>{second entry creator}<tab>...<newline>
     {second entry title}<tab>{second entry creator}<tab>...<newline>
 
-    and also returns the formatted dataframe.
-
-    :param df: The uncleaned dataframe in format provided by
-               National Library of Scotland
-    :param file_path: File path of original data, to name output files with
-    :param filter_date: Date in year format (e.g. 1863) to filter, +/- 1 year
-    :return pd.DataFrame: Cleaned entries, filtered down by date
-    :return pd.DataFrame: Cleaned entries that have no interpretable date
+    :param df: The dataframe in format provided by National Library of Scotland
+    :param file_path: File path of original data, to name debug output files
+    :param debug: if True then save the dataframe out as a tsv file
+    :return pd.DataFrame: The columnar dataframe
     """
-
-    out_dir = file_path.parent.joinpath(file_path.stem + "_clean")
-    out_dir.mkdir(parents=True, exist_ok=True)
-
     # Strip out the data key, but leave other colons found in value
-    df = df.map(lambda x: ':'.join(x.split(':')[1:]).rstrip('/').strip())
+    df = df.map(lambda x: ':'.join(x.split(':')[1:]))
     labels = [
         'Title', 'Creator', 'Type', 'Publisher', 'Date', 'Language', 'Format',
         'Relation', 'Rights', 'Identifier', 'Description', 'Subject',
@@ -67,56 +83,111 @@ def clean_nls_dates(df: pd.DataFrame, file_path: os.PathLike,
     ]
     df.columns = labels
     if debug:
+        out_dir = file_path.parent.joinpath(file_path.stem + "_clean")
+        out_dir.mkdir(parents=True, exist_ok=True)
         df.to_csv(_labelled_file(out_dir, file_path, 'columnar'),
                   sep='\t',
                   index=False)
+    return df
+
+
+def clean_nls_titles(df: pd.DataFrame, file_path: os.PathLike,
+                     debug: bool) -> pd.DataFrame:
+    """
+    Collecting the different title cleaning functions here
+
+    :param df: The dataframe with uncleaned titles in columnar format
+    :param file_path: File path of original data, to name debug output files
+    :param debug: if True then save the dataframe out as a tsv file
+    :return pd.DataFrame: The columnar dataframe
+    """
+    df['clean_title'] = (
+        df['Title'].map(_remove_metadata).map(_clean_title_string))
+    if debug:
+        out_dir = file_path.parent.joinpath(file_path.stem + "_clean")
+        out_dir.mkdir(parents=True, exist_ok=True)
+        df.to_csv(_labelled_file(out_dir, file_path, 'clean_titles'),
+                  sep='\t',
+                  index=False)
+    return df
+
+
+def clean_nls_dates(df: pd.DataFrame, file_path: os.PathLike,
+                    debug: bool) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Clean the dates of the National Library of Scotland dataset.
+    The dates have multiple annotations e,g, c1983, circa 1983 etc.
+    Try to identify and group these and give columns for min and
+    max dates
+
+    :param df: The uncleaned dataframe in format provided by
+               National Library of Scotland
+    :param file_path: File path of original data, to name debug files
+    :param debug: if True then save the dataframe out as a tsv file
+    :return pd.DataFrame: Cleaned entries
+    """
 
     df_len = len(df)
-    logging.info(f'No. of entries: {df_len}')
 
     # Separate out different types of date in case they're relevant
     dates_re = (r'(?:c(?:a\.?|irca|) ?(?P<circa_date>\d{4})|'
                 r'(?P<question_date>\d{4})\?|'
                 r'(?P<unqualified_date>\d{4}))')
+
+    # create a dataframe of date matches with original index and match index
+    # and match index as a multi-index
     dates_df = df['Date'].str.extractall(dates_re).astype('float64')
     if debug:
+        out_dir = file_path.parent.joinpath(file_path.stem + "_clean")
+        out_dir.mkdir(parents=True, exist_ok=True)
         dates_df.to_csv(
-            _labelled_file(out_dir, file_path, 'dates'),
+            _labelled_file(out_dir, file_path, 'datetypes'),
             sep='\t',
         )
 
+    # Grab the question and circa dates with their original indices
     question_dates = dates_df.pop('question_date').groupby(
         level=0).first().dropna()
     n_qd = len(question_dates)
-    logging.info(f'No. of question marked dates: {n_qd}')
 
     circa_dates = dates_df.pop('circa_date').groupby(level=0).first().dropna()
     n_cd = len(circa_dates)
-    logging.info(f'No. of circa marked dates: {n_cd}')
 
+    # Grab the unqualified dates with original indices into one series,
+    # if there are more than one, take the lowest
     min_uq_dates = dates_df.groupby(level=0).min().rename(
         columns={
             'unqualified_date': 'min_uq_date'
         }).dropna()
+
+    # Grab the unqualified dates with original indices into one series,
+    # if there are more than one, take the highest
     max_uq_dates = dates_df.groupby(level=0).max().rename(
         columns={
             'unqualified_date': 'max_uq_date'
         }).dropna()
 
+    # Make a new empty dataframe to hold the sorted dates data
+    # the new frame has the same size/index as the original
     processed_dates = pd.DataFrame(
         np.nan,
         index=range(df_len),
         columns=['question_date', 'circa_date', 'min_uq_date', 'max_uq_date'])
+
+    # Insert the various dates at their labelled indices
     processed_dates.update(question_dates)
     processed_dates.update(circa_dates)
     processed_dates.update(min_uq_dates['min_uq_date'])
     processed_dates.update(max_uq_dates['max_uq_date'])
 
+    # Make a new empty dataframe to hold the reduced dates data
+    # the new frame has the same size/index as the original
     date_range = pd.DataFrame(np.nan,
                               index=range(df_len),
                               columns=['min_date', 'max_date'])
 
     # NB: Effectively ignoring different date types for now
+    # Just grab the min and max dates across all types
     date_range['min_date'] = processed_dates.min(axis=1)
     date_range['max_date'] = processed_dates.max(axis=1)
 
@@ -124,31 +195,53 @@ def clean_nls_dates(df: pd.DataFrame, file_path: os.PathLike,
     # processed_dates = processed_dates.map(
     #     lambda x: pd.to_datetime(x, format='%Y', errors='coerce'))
 
-    if debug:
-        processed_dates.to_csv(
-            _labelled_file(out_dir, file_path, 'processed_dates'),
-            sep='\t',
-        )
-
     df = pd.concat(
         [df.loc[:, :'Date'], processed_dates, df.loc[:, 'Language':]], axis=1)
 
-    df.to_csv(_labelled_file(out_dir, file_path, 'clean'),
-              sep='\t',
-              index=False)
+    logging.info(f'No. of entries: {df_len}')
+    logging.info(f'No. of question marked dates: {n_qd}')
+    logging.info(f'No. of circa marked dates: {n_cd}')
 
-    # TODO: split function here
+    if debug:
+        out_dir = file_path.parent.joinpath(file_path.stem + "_clean")
+        out_dir.mkdir(parents=True, exist_ok=True)
+        processed_dates.to_csv(_labelled_file(out_dir, file_path,
+                                              'processed_dates'),
+                               sep='\t')
+        df.to_csv(_labelled_file(out_dir, file_path, 'cleaned_dates'),
+                  sep='\t',
+                  index=False)
+
+    return df
+
+
+def filter_nls_date(df: pd.DataFrame,
+                    filter_date: int,
+                    file_path: os.PathLike,
+                    debug: bool) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Filter out dates +/- one year from 'filter_date'
+
+    :param df: The full dataframe of entries with min and max dates
+    :param file_path: File path of original data, to name debug files
+    :param debug: if True then save the dataframe out as a tsv file
+    :return filtered dataframe and a separate dataframe of undated entries
+    """
+
     register_df = df.loc[((df['min_date'] - 1.1) < filter_date)
                          & ((df['max_date'] + 1.1) > filter_date)]
-    register_df.to_csv(_labelled_file(out_dir, file_path,
-                                      'filtered_' + str(filter_date)),
-                       sep='\t',
-                       index=False)
+    missing_df = df.loc[df['min_date'].isnull() & df['max_date'].isnull()]
 
-    missing_df = df.loc[df['min_date'].isnull()]
-    missing_df.to_csv(_labelled_file(out_dir, file_path, 'missing'),
-                      sep='\t',
-                      index=False)
+    if debug:
+        out_dir = file_path.parent.joinpath(file_path.stem + "_clean")
+        out_dir.mkdir(parents=True, exist_ok=True)
+        register_df.to_csv(_labelled_file(out_dir, file_path,
+                                          'filtered_' + str(filter_date)),
+                           sep='\t',
+                           index=False)
+        missing_df.to_csv(_labelled_file(out_dir, file_path, 'missing'),
+                          sep='\t',
+                          index=False)
 
     n_exact = len(df.loc[((df['min_date'] - 0.9) < filter_date)
                          & ((df['max_date'] + 0.9) > filter_date)])
@@ -192,6 +285,7 @@ def main(folder: str, debug: bool) -> None:
     register_date = 1863
     register_df = pd.DataFrame()
     missing_df = pd.DataFrame()
+
     for file_path in file_paths:
         print(f"Processing: {file_path}")
         df = pd.read_csv(file_path,
@@ -199,9 +293,19 @@ def main(folder: str, debug: bool) -> None:
                          engine='python',
                          on_bad_lines=partial(lambda line: line[:15]))
         try:
-            new_register_df, new_missing_df = clean_nls_dates(df,
-                                                              file_path,
+            df = (df.pipe(columnise_nls_data,
+                          file_path=file_path,
+                          debug=debug)
+                  .pipe(clean_nls_titles,
+                        file_path=file_path,
+                        debug=debug)
+                  .pipe(clean_nls_dates,
+                        file_path=file_path,
+                        debug=debug))
+
+            new_register_df, new_missing_df = filter_nls_date(df,
                                                               register_date,
+                                                              file_path,
                                                               debug=debug)
             register_df = pd.concat([register_df, new_register_df])
             missing_df = pd.concat([missing_df, new_missing_df])
