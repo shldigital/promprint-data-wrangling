37a769382fa96d2b129974c0b3fba8b44d12d20c
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-11-12 17:28:07 +0000

    [feat] Organise scripts as a cli

---
+++ cli/clean_nls.py
@@ -0,0 +1,93 @@
+import argparse
+import glob
+import helpers
+import logging
+import nls
+import pandas as pd
+
+from functools import partial
+from pathlib import Path
+from typing import Any
+
+
+logger = logging.getLogger('')
+logging.basicConfig(level=logging.INFO,
+                    filename="promprint-cleaning.log",
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filemode='w')
+
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+
+
+def main(folder: str, debug: bool = False, **kwargs: Any) -> None:
+    file_paths = map(Path,
+                     glob.glob(folder + '*.tsv') + glob.glob(folder + '*.txt'))
+    registers = {"1863b": 1863, "undated": None}
+    date_range = 1.
+    compiled_df = pd.DataFrame()
+
+    # N.B. We're doing cleaning per file so that a human can inspect
+    # debug output easier (per file instead of the entire compilation)
+    for file_path in file_paths:
+        print(f"Processing: {file_path}")
+        # `on_bad_lines` deals with the errant tabs at end of nls data files
+        df = pd.read_csv(file_path,
+                         sep='\t',
+                         engine='python',
+                         on_bad_lines=partial(lambda line: line[:15]))
+        df = (df.pipe(nls.columnise_nls_data,  # type: ignore[call-overload]
+                      file_path=file_path,
+                      debug=debug)
+              .pipe(helpers.clean_titles,
+                    file_path=file_path,
+                    debug=debug)
+              .pipe(nls.clean_nls_dates,
+                    file_path=file_path,
+                    debug=debug))
+        compiled_df = pd.concat([compiled_df, df])
+
+    print(f"Total No. of entries: {len(compiled_df)}")
+
+    if debug:
+        clean_path = Path(folder).parent.joinpath(
+            folder.rstrip("/") + "_clean.tsv")
+        compiled_df.to_csv(clean_path, sep='\t', index=False)
+
+    for register_name, register_date in registers.items():
+        register_df = nls.filter_nls_date(compiled_df,
+                                          register_date,
+                                          date_range,
+                                          folder,
+                                          debug=debug)
+
+        print(f"No. of entries after filtering for register {register_name}"
+              f": {len(register_df)}")
+
+        source_library = 'NLS'
+        register_df = helpers.format_library_set(register_df,
+                                                 None,
+                                                 source_library,
+                                                 register_name)
+        register_path = Path(folder).parent.joinpath(
+            folder.rstrip("/") + "_filtered_" + register_name + "_db.tsv")
+        register_df.to_csv(register_path, sep='\t', index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder', help='Folder of input files in tsv format')
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help='Save intermediate stages of cleaning to file')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        console.setLevel(logging.INFO)
+    else:
+        console.setLevel(logging.WARNING)
+    logging.getLogger('').addHandler(console)
+
+    main(args.folder, args.debug)
--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import argparse
-import glob
 import logging
 import os
-import pandas as pd
 import sys
 
-from functools import partial
-from pathlib import Path
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'cli'))
 import helpers
 import nls
+from cli import clean_nls
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('')
 logging.basicConfig(level=logging.INFO,
                     filename="promprint-cleaning.log",
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -23,68 +20,24 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 console.setFormatter(formatter)
 
 
-def main(folder: str, debug: bool) -> None:
-
-    file_paths = map(Path,
-                     glob.glob(folder + '*.tsv') + glob.glob(folder + '*.txt'))
-    registers = {"1863b": 1863, "undated": None}
-    date_range = 1.
-    compiled_df = pd.DataFrame()
-
-    # N.B. We're doing cleaning per file so that a human can inspect
-    # debug output easier (per file instead of the entire compilation)
-    for file_path in file_paths:
-        print(f"Processing: {file_path}")
-        # `on_bad_lines` deals with the errant tabs at end of nls data files
-        df = pd.read_csv(file_path,
-                         sep='\t',
-                         engine='python',
-                         on_bad_lines=partial(lambda line: line[:15]))
-        df = (df.pipe(nls.columnise_nls_data,  # type: ignore[call-overload]
-                      file_path=file_path,
-                      debug=debug)
-              .pipe(helpers.clean_titles,
-                    file_path=file_path,
-                    debug=debug)
-              .pipe(nls.clean_nls_dates,
-                    file_path=file_path,
-                    debug=debug))
-        compiled_df = pd.concat([compiled_df, df])
-
-    print(f"Total No. of entries: {len(compiled_df)}")
-
-    if debug:
-        clean_path = Path(folder).parent.joinpath(
-            folder.rstrip("/") + "_clean.tsv")
-        compiled_df.to_csv(clean_path, sep='\t', index=False)
-
-    for register_name, register_date in registers.items():
-        register_df = nls.filter_nls_date(compiled_df,
-                                          register_date,
-                                          date_range,
-                                          folder,
-                                          debug=debug)
-
-        print(f"No. of entries after filtering for register {register_name}"
-              f": {len(register_df)}")
-
-        source_library = 'NLS'
-        register_df = helpers.format_library_set(register_df,
-                                                 None,
-                                                 source_library,
-                                                 register_name)
-        register_path = Path(folder).parent.joinpath(
-            folder.rstrip("/") + "_filtered_" + register_name + "_db.tsv")
-        register_df.to_csv(register_path, sep='\t', index=False)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('folder', help='Folder of input files in tsv format')
-    parser.add_argument('--debug',
+def main():
+    parser = argparse.ArgumentParser(
+        description="General scripts for cleaning promprint data at various stages")
+    subparsers = parser.add_subparsers(help="Datasets to manipulate")
+    parser.add_argument("-d", "--debug",
                         action='store_true',
                         help='Save intermediate stages of cleaning to file')
 
+    nls_parser = subparsers.add_parser('nls')
+    nls_parser.add_argument('folder',
+                            type=str,
+                            help='Folder of input files in tsv format')
+    nls_parser.set_defaults(func=clean_nls.main)
+
+    register_parser = subparsers.add_parser("register")
+    register_parser.add_argument('file',
+                                 type=str,
+                                 help="File to clean, in csv format")
     args = parser.parse_args()
 
     if args.debug:
@@ -93,4 +46,8 @@ if __name__ == "__main__":
         console.setLevel(logging.WARNING)
     logging.getLogger('').addHandler(console)
 
-    main(args.folder, args.debug)
+    args.func(**vars(args))
+
+
+if __name__ == "__main__":
+    main()
