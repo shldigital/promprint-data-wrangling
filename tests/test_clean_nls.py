db26738c930cf8010373398c46c540dc0a8d2e5e
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-11-21 10:12:39 +0000

    [tests] remove unused variable

--- tests/test_clean_nls.py
+++ tests/test_clean_nls.py
@@ -3,7 +3,6 @@ import glob
 from src.cli.clean_nls import main
 
 input_folder = "./tests/test_files/test_nls/"
-dated_filename = "test_nls_1863b_export.tsv"
 
 
 def test_returns_tsv_file(tmp_path):
