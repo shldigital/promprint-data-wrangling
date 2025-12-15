e1c023e14cf22d5235fc77c67294ef3d69f454a0
Nic Seymour-Smith <nrs23@sussex.ac.uk>
2025-12-15 15:29:50 +0000

    [feat] add new registers to output

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
     "NLS": {
         "registers": {
             "1863b": 1863,
+            "1886b": 1886,
+            "1907a|1907b": 1907,
             "undated": None
         },
         "date_range": 1.
